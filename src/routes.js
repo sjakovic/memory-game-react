16656232bb64873de98a3164af31f52b8344a7ec
Simo Jaković <simo.jakovic@homegate.ch>
2019-03-04 15:29:41 +0100

    Refaktoring

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ import App from './components/App';
 import HomePage from './components/home/HomePage';
 
 export default (
-  // Route path for production build this must be a path to subfolder if project is not in root directory
+  // Route path for production build must be a path to subfolder if project is not in root directory
   <Route path="/" component={App}>
     <IndexRoute component={HomePage}/>
   </Route>
